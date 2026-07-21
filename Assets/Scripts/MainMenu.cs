136eccde1684e89bc76147d9f2306146ad61ec68
oscarlzx2002 <leeoscar0603@gmail.com>
2026-07-22 05:11:25 +0800

    winScene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,9 +7,18 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
     public void QuitApp()
     {
         Application.Quit();
         Debug.Log("Application has quit.");
+    }
+    public void RetryGame()
+    {
+        // Reloads whichever scene is currently active
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
